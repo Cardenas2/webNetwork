9d75098afab02904ae5ca071778e0855a276fc2d
Cardenas2 <vaniacardenas-21@hotmail.com>
2020-11-20 14:57:11 -0300

    trabajar codigo js para scroll y cambiar caracteristicas de menu a una altuira de 80px, tambien agregar clase fondo menu con background diferente y padding distinto con transicion

--- js/sitio.js
+++ js/sitio.js
@@ -9,3 +9,32 @@ var easeInQuad = new SmoothScroll('[data-easing="easeInQuad"]', {
 });
 
 
+/*---------------------------------
+    OCULTAR Y MOSTRAR BOTON IR ARRIBA
+ ----------------------------------*/
+ $(function () {
+    $(window).scroll(function () {
+        var scrolltop = $(this).scrollTop();
+        if (scrolltop >= 100) {
+            $(".icono-flechaIrArriba").fadeIn();
+        } else {
+            $(".icono-flechaIrArriba").fadeOut();
+        }
+    });
+
+});
+
+/*---------------------------------
+   CABECERA ANIMADA
+ ----------------------------------*/
+ $(window).scroll(function () {
+
+    var nav = $('.bienvenidos__encabezado');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+        nav.addClass("fondo-menu");
+    } else {
+        nav.removeClass("fondo-menu");
+    }
+});
